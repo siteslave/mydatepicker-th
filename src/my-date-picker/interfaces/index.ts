662058a36e3ecdd2e3d8f625406e5c285638a995
kekeh <kea@live.fi>
2016-07-20 20:03:52 +0300

    Changed order of the my-options.interface and the my-locale.interface in the interfaces/index.ts file

--- src/my-date-picker/interfaces/index.ts
+++ src/my-date-picker/interfaces/index.ts
@@ -3,6 +3,5 @@ export * from './my-day-labels.interface';
 export * from './my-month-labels.interface';
 export * from './my-month.interface';
 export * from './my-week.interface';
-export * from './my-locale.interface';
 export * from './my-options.interface';
-
+export * from './my-locale.interface';
