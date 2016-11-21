7fc9be0b180b04ee9c7e7045c18dcdcb548166d4
Artem Ibatullin <artem_ibatullin@itsl.ru>
2016-11-21 15:47:04 +0300

    Add russian language support

--- dist/services/my-date-picker.locale.service.js
+++ dist/services/my-date-picker.locale.service.js
@@ -74,6 +74,14 @@ var LocaleService = (function () {
                 todayBtnTxt: "Vandaag",
                 firstDayOfWeek: "mo",
                 sunHighlight: false
+            },
+            "ru": {
+                dayLabels: { su: "Вс", mo: "Пн", tu: "Вт", we: "Ср", th: "Чт", fr: "Пт", sa: "Сб" },
+                monthLabels: { 1: "Янв", 2: "Фев", 3: "Март", 4: "Апр", 5: "Май", 6: "Июнь", 7: "Июль", 8: "Авг", 9: "Сент", 10: "Окт", 11: "Ноя", 12: "Дек" },
+                dateFormat: "dd.mm.yyyy",
+                todayBtnTxt: "Сегодня",
+                firstDayOfWeek: "mo",
+                sunHighlight: true,
             }
         };
     }
