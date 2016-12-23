e2ac9adb98a449b7c7242a4d950f83598cfa0f6b
Radoslav Konuch <Radoslav.Konuch@anritsu.com>
2016-12-23 20:36:19 +0100

    Added slovak locale config.

--- src/my-date-picker/services/my-date-picker.locale.service.ts
+++ src/my-date-picker/services/my-date-picker.locale.service.ts
@@ -130,6 +130,14 @@ export class LocaleService {
             todayBtnTxt: "ယနေ့",
             firstDayOfWeek: "mo",
             sunHighlight: true,
+        },
+        "sk": {
+            dayLabels: { su: "Ne", mo: "Po", tu: "Ut", we: "St", th: "Št", fr: "Pi", sa: "So" },
+            monthLabels: { 1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "Máj", 6: "Jún", 7: "Júl", 8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec" },
+            dateFormat: "dd.mm.yyyy",
+            todayBtnTxt: "Dnes",
+            firstDayOfWeek: "mo",
+            sunHighlight: true,
         }
     };
 
