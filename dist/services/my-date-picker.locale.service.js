0066c3902aa4ab08606115a311750fefcaec8c50
kekeh <kea@live.fi>
2017-01-16 16:29:49 +0200

    Added it-ch locale.

--- dist/services/my-date-picker.locale.service.js
+++ dist/services/my-date-picker.locale.service.js
@@ -118,6 +118,14 @@ var LocaleService = (function () {
             "it": {
                 dayLabels: { su: "Dom", mo: "Lun", tu: "Mar", we: "Mer", th: "Gio", fr: "Ven", sa: "Sab" },
                 monthLabels: { 1: "Gen", 2: "Feb", 3: "Mar", 4: "Apr", 5: "Mag", 6: "Giu", 7: "Lug", 8: "Ago", 9: "Set", 10: "Ott", 11: "Nov", 12: "Dic" },
+                dateFormat: "dd/mm/yyyy",
+                todayBtnTxt: "Oggi",
+                firstDayOfWeek: "mo",
+                sunHighlight: true
+            },
+            "it-ch": {
+                dayLabels: { su: "Dom", mo: "Lun", tu: "Mar", we: "Mer", th: "Gio", fr: "Ven", sa: "Sab" },
+                monthLabels: { 1: "Gen", 2: "Feb", 3: "Mar", 4: "Apr", 5: "Mag", 6: "Giu", 7: "Lug", 8: "Ago", 9: "Set", 10: "Ott", 11: "Nov", 12: "Dic" },
                 dateFormat: "dd.mm.yyyy",
                 todayBtnTxt: "Oggi",
                 firstDayOfWeek: "mo",
