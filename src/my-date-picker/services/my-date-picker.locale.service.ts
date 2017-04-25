979926b73910294e47d285bb3a9b1c190ba00610
Satit Rianpit <siteslave@users.noreply.github.com>
2017-04-25 11:57:24 +0700

    add `th` for Thai locale

--- src/my-date-picker/services/my-date-picker.locale.service.ts
+++ src/my-date-picker/services/my-date-picker.locale.service.ts
@@ -242,6 +242,14 @@ export class LocaleService {
             todayBtnTxt: "Бүгін",
             firstDayOfWeek: "mo",
             sunHighlight: true
+        },
+        "th": {
+            dayLabels: { su: "อา", mo: "จ", tu: "อ", we: "พ", th: "พฤ", fr: "ศ", sa: "ส" },
+            monthLabels: { 1: "ม.ค", 2: "ก.พ.", 3: "มี.ค.", 4: "เม.ย.", 5: "พ.ค.", 6: "มิ.ย.", 7: "ก.ค.", 8: "ส.ค.", 9: "ก.ย.", 10: "ต.ค.", 11: "พ.ย.", 12: "ธ.ค." },
+            dateFormat: "dd-mm-yyyy",
+            todayBtnTxt: "วันนี้",
+            firstDayOfWeek: "su",
+            sunHighlight: true   
         }
     };
 
